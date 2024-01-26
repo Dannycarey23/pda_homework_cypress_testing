bdffc91ec6cd3b8511b6d49e1864be7e363f2fe0
Danny <carey.d@ntlworld.com>
2024-01-26 11:53:30 +0000

    added integration tests as requested

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -19,7 +19,7 @@ describe("Calculator", () => {
     cy.get('#number7').click();
     cy.get('#number8').click();
     cy.get('#number9').click();
-    cy.get('.display').should('contain','1023456789')
+    cy.get('.display').should('contain', '1023456789')
   })
 
   it('the arithmetical operations update the display with the result of the operation', () => {
@@ -38,7 +38,7 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '6')
-    
+
   })
 
   it('if you divide by zero, the answer should be 0', () => {
@@ -48,5 +48,145 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '0')
   })
+
+  it('decimal numbers can be multiplied', () => {
+    cy.get('#number2').click();
+    cy.get('#decimal').click();
+    cy.get('#number5').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '5')
+  })
+
+  it('decimal numbers can be divided', () => {
+    cy.get('#number6').click();
+    cy.get('#decimal').click();
+    cy.get('#number4').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '3.2')
+  })
+
+  it('decimal numbers can be added', () => {
+    cy.get('#number6').click();
+    cy.get('#decimal').click();
+    cy.get('#number4').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#decimal').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '8.7')
+  })
+
+  it('decimal numbers can be subtracted', () => {
+    cy.get('#number6').click();
+    cy.get('#decimal').click();
+    cy.get('#number4').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number2').click();
+    cy.get('#decimal').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '4.1')
+  })
+
+  it('large numbers can be added', () => {
+    cy.get('#number2').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '4000000')
+  })
+  it('large numbers can be subtracted', () => {
+    cy.get('#number2').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '1000000')
+  })
+  it('large numbers can be multiplied', () => {
+    cy.get('#number2').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number3').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '6000000')
+  })
+  it('large numbers can be divided', () => {
+    cy.get('#number8').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number4').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '2')
+  })
+
+  it('be able to display negative numbers', () => {
+    cy.get('#number2').click();
+    cy.get('#number0').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number3').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-10')
+  })
+
+  it('dividing by zero should display 0', () => {
+    cy.get('#number5').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '0')
+  })
+
 
 })
